3074a701aac87fe49690057b76c87085977e7e07
ZKD <kuperz@gmail.com>
2018-02-12 07:54:07 +0200

    db migration scripts

--- public/admin/prd/UpdateItems_skillera-prd.js
+++ public/admin/prd/UpdateItems_skillera-prd.js
@@ -1,3 +1,3 @@
 db = connect("ds229458.mlab.com:29458/heroku_h5ztbh0x");
 db.auth( {user: "skillera-prd", pwd: "Mlab0099"});
-db.items.createIndex({"skill.type":"text"});
+db.items.createIndex({"skill":"text"});
--- public/admin/qa/UpdateItems_skillera-qa.js
+++ public/admin/qa/UpdateItems_skillera-qa.js
@@ -1,3 +1,3 @@
 db = connect("ds229468.mlab.com:29468/heroku_dwwt61r2");
 db.auth( {user: "skillera-qa", pwd: "Mlab0099"});
-db.items.createIndex({"skill.type":"text"});
+db.items.createIndex({"skill":"text"});
