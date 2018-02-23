3b4c8b434c10f9df44c3a3bab70a246dbd2cd860
Hadar <hadarg1@gmail.com>
2018-02-23 11:21:02 +0200

    Fix collection name

--- public/admin/prd/UpdateApplications_prd.js
+++ public/admin/prd/UpdateApplications_prd.js
@@ -1,3 +1,3 @@
 db = connect("ds229458.mlab.com:29458/heroku_h5ztbh0x");
 db.auth( {user: "skillera-prd", pwd: "Mlab0099"});
-db.companies.updateMany({}, {$set: {fraudType : "No Fraud"}});
+db.applications.updateMany({}, {$set: {fraudType : "No Fraud"}});
