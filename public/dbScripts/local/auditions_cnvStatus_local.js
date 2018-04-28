01297e538eb3b6cefa7de177e958ffd7c751da3b
ZKD <kuperz@gmail.com>
2018-04-28 13:07:59 +0300

    ski-83-1 improve linkedIn URL href ; clear all applications red errors

--- public/dbScripts/local/auditions_cnvStatus_local.js
+++ public/dbScripts/local/auditions_cnvStatus_local.js
@@ -1,7 +1,7 @@
 db = connect("localhost:3001/meteor");
 
 db.auditions.find().forEach(function(audition){
-	if (audition.status === 'Verified') {
+	if (audition.status === 'Assigned') {
 		campaignRec = db.campaigns.findOne({_id: audition.campaignId});
 		if (campaignRec.status !== 'Published') {
 			db.auditions.update({_id: audition._id}, {$set: {status: 'Verified'}})
