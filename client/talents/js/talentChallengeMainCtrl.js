ce454273e0cd283ce8b4e9b1f8ae491a57291ef6
Hadar <hadarg1@gmail.com>
2018-05-04 14:48:11 +0300

    [SKI-103] DE - update "available" challenge

--- client/talents/js/talentChallengeMainCtrl.js
+++ client/talents/js/talentChallengeMainCtrl.js
@@ -463,6 +463,9 @@ angular
                                     };
                                     case ENUM.MODAL_RESULT.CLOSE: {
                                         
+                                        if ((vm.editItem.status === ENUM.ITEM_STATUS.IN_WORK) || (vm.editItem.status === ENUM.ITEM_STATUS.AVAILABLE)) {
+                                            vm.editItem = null;
+                                        };
                                         break;
                                     };
                                     default:
